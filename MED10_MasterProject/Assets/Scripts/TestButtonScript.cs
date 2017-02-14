edee60b3c276984e36710a032fe24124e68ea95b
LasseR <lasse_1302@hotmail.com>
2017-02-14 12:58:23 +0100

    Made a singleton which controls the data in the UI

---
+++ MED10_MasterProject/Assets/Scripts/TestButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Script.UIBar;
+
+public class TestButtonScript : MonoBehaviour {
+    public Text inputField;
+
+
+
+
+    public void ButtonPress()
+    {
+        var test = UIData.Instance;
+        Debug.Log(test.AccountBalance);
+        test.AccountBalance = int.Parse(inputField.text);
+
+    }
+
+	
+}
