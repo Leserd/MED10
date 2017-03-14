b57b597b44bcb5bb4c67147394bd1117d54fddc7
Andreas Bork <Ahbork@gmail.com>
2017-03-14 14:14:08 +0100

    Simple hint system has been set up, you create a hint using a sprite from Resources + a position to place the hint.

---
+++ MED10_MasterProject/Assets/Scripts/HintManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            new Hint("Sprites/Hints/NotAvailableSprite", new Vector3(50,-50,0));
+        }
+	}
+}
