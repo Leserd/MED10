b97670f66d2fe3b5ec2fc970c91b97895dd458a8
LasseR <lasse_1302@hotmail.com>
2017-03-23 10:16:44 +0100

    Start editBar

---
+++ MED10_MasterProject/Assets/EditBill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditBill : MonoBehaviour {
+    string IDnum;
+
+	void Awake () {
+		
+	}
+
+    void EventStart(GameObject building)
+    {
+        if (building != null && building.GetComponent<Building>())
+        {
+            IDnum = building.GetComponent<Building>()
+        }
+    }
+	
+
+}
