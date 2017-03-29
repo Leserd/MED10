a55b8093a6fccc85682d9b50d78a20e37f678640
LasseR <lasse_1302@hotmail.com>
2017-03-29 12:32:58 +0200

    Added charts to the project. Takes betalingsservicedata as input

---
+++ MED10_MasterProject/Assets/Graphs/Charts/Scripts/BarChart.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using DataVisualisation.Utilities;
+using System.Collections.Generic;
+
+public class BarChart : MonoBehaviour
+{
+    public Bar barPrefab;
+
+    public float Chartheight { get; set; }
+
+    public void SetData(List<BetalingsServiceData.BSData> dataCollection)
+    {
+        float maxValue = 0;
+
+        foreach (var entry in dataCollection)
+        {
+            if (entry.MonthlyExpence > maxValue)
+                maxValue = entry.MonthlyExpence;
+        }
+
+        var colors = ColorGenerator.GetColorsGoldenRatio(dataCollection.Count);
+        for (int i = 0; i < dataCollection.Count; i++)
+        {
+            Bar newBar = Instantiate(barPrefab) as Bar;
+            newBar.transform.SetParent(transform);
+
+            var data = dataCollection[i];
+            float value = data.MonthlyExpence;
+
+            // set size of bar/s
+            RectTransform rt = newBar.bar.GetComponent<RectTransform>();
+            float normalizedValue = (value / maxValue) * 0.95f;
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, Chartheight * normalizedValue);
+            newBar.bar.color = colors[i];
+
+            newBar.label.text = data.TransactionName;
+
+            //set value label
+            newBar.barValue.text = value.ToString();
+            if (rt.sizeDelta.y < 30f)
+            {
+                newBar.barValue.rectTransform.pivot = new Vector2(0.5f, 0f);
+                newBar.barValue.rectTransform.anchoredPosition = Vector2.zero;
+            }
+        }
+    }
+}
---
+++ MED10_MasterProject/Assets/Graphs/Charts/Scripts/PieGraph.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using DataVisualisation.Utilities;
+
+
+public class PieGraph : MonoBehaviour
+{
+    public Pie PiePrefab;
+
+    public void SetData(List<BetalingsServiceData.BSData> dataCollection)
+    {
+       
+
+        var total = 0f;
+        var zRotation = 0f;
+
+        for (var i = 0; i < dataCollection.Count; i++)
+        {
+            total += dataCollection[i].MonthlyExpence;
+        }
+
+        var colors = ColorGenerator.GetColorsGoldenRatio(dataCollection.Count);
+        for (var i = 0; i < dataCollection.Count; i++)
+        {
+            var entry = dataCollection[i];
+            
+            //Create the pie
+            Pie newWedge = Instantiate(PiePrefab) as Pie;
+            newWedge.transform.SetParent(transform);
+            newWedge.pie.transform.SetParent(transform, false);
+            newWedge.pie.GetComponent<RectTransform>().sizeDelta = transform.root.GetComponent<RectTransform>().sizeDelta;
+            newWedge.pie.color = colors[i];
+            newWedge.pie.fillAmount = entry.MonthlyExpence / total;
+            newWedge.pie.transform.rotation = Quaternion.Euler(new Vector3(0, 0, zRotation));
+            zRotation -= newWedge.pie.fillAmount * 360f;
+
+            //Create the correct labels
+            newWedge.label.text = entry.TransactionName;
+            newWedge.label.transform.localEulerAngles = new Vector3(0, 0, -newWedge.pie.GetComponent<RectTransform>().localEulerAngles.z);
+
+            //Create the correct percentage values
+            newWedge.pieValue.text = (Math.Round(entry.MonthlyExpence / total,3)).ToString();
+            newWedge.pieValue.transform.localEulerAngles = new Vector3 (0,0,-newWedge.pie.GetComponent<RectTransform>().localEulerAngles.z);
+        }
+        
+    }
+}
